25b122a73da893b85d15ac471547d0d893141c5d
Justin Dennison <justin1dennison@gmail.com>
2017-06-28 21:57:06 -0400

    intermediate commit for review at the end of the day 6.28.17

--- routers/weather.router.js
+++ routers/weather.router.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const darksky = require('../credentials').darksky; //be careful!!!
+const baseUrl = `https://api.darksky.net/forecast/${darksky}/37.8267,-122.4233`;
+const axios = require('axios');
 router.get('/weather', (request, response) => {
-  response.send('Here be the weather');
+  axios.get(baseUrl)
+       .then(weather => {
+         response.json(weather.data);
+       })
+       .catch(err => {});
 });
 router.get('/weather/:lat,:lon', (request, response) => {
   response.send('Here be the weather for that place');
