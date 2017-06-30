d7b6ab91694cdb67524807bc69c49ad7574f0a57
Justin Dennison <justin1dennison@gmail.com>
2017-06-29 20:43:32 -0400

    not found middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8080;
 
 //middleware imports
 const logger = require('./middleware/logger');
+const notFound = require('./middleware/404');
 //routers
 const weatherRouter = require('./routers/weather.router');
 
@@ -14,7 +15,7 @@ server.use(weatherRouter);
 server.get('/', (request, response) => {
   response.send('it works!');
 });
-
+server.use(notFound);
 server.listen(port, () => {
   console.log('Now listening on port: ', port);
 });
