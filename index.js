6866863cc23d737b344c9c94ea689d58627a1cac
Justin Dennison <justin1dennison@gmail.com>
2017-06-29 20:57:51 -0400

    created and wired up a generic error handler

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 //middleware imports
 const logger = require('./middleware/logger');
 const notFound = require('./middleware/404');
+const errorHandler = require('./middleware/500');
 //routers
 const weatherRouter = require('./routers/weather.router');
 
@@ -16,6 +17,7 @@ server.get('/', (request, response) => {
   response.send('it works!');
 });
 server.use(notFound);
+server.use(errorHandler);
 server.listen(port, () => {
   console.log('Now listening on port: ', port);
 });
--- routers/weather.router.js
+++ routers/weather.router.js
@@ -4,17 +4,17 @@ const darksky = process.env.DARKSKY || require('../credentials').darksky; //be c
 const baseUrl = `https://api.darksky.net/forecast/${darksky}/`;
 const axios = require('axios');
 
-router.get('/weather', (request, response) => {
+router.get('/weather', (request, response, next) => {
   const url = `${baseUrl}29,-82`;
   axios.get(url)
        .then(weather => {
          response.json(weather.data);
        })
        .catch(err => {
-         console.error(err);
+         next(err);
        });
 });
-router.get('/weather/:lat,:lon', (request, response) => {
+router.get('/weather/:lat,:lon', (request, response, next) => {
   const lat = request.params.lat;
   const lon = request.params.lon;
   const url = `${baseUrl}${lat},${lon}`;
@@ -23,7 +23,7 @@ router.get('/weather/:lat,:lon', (request, response) => {
           response.json(weather.data);
         })
         .catch(err => {
-          console.error(err);
+          next(err);
         });
 });
 module.exports = router;
