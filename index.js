59aea3a98a61c049f104106343c4f4e2174ca87c
Justin Dennison <justin1dennison@gmail.com>
2017-06-29 20:08:57 -0400

    created and wired up a custom logging middleware function

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 8080;
 
+//middleware imports
+const logger = require('./middleware/logger');
 //routers
 const weatherRouter = require('./routers/weather.router');
+
+//middleware use
+server.use(logger);
 server.use(weatherRouter);
 //dummy route for testing
 server.get('/', (request, response) => {
---
+++ middleware/logger.js
@@ -0,0 +1,10 @@
+const logger = (request, response, next) => {
+  const url = request.url;
+  const method = request.method;
+  const now = new Date();
+  const message = `${method}: ${url} - ${now}`;
+  console.log(message);
+  next();
+};
+
+module.exports = logger;
