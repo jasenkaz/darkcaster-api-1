afad9e82e57747f5cee25797ce46dc612dd9d58f
Justin Dennison <justin1dennison@gmail.com>
2017-07-12 19:45:33 -0400

    added cors middleware to enable cors

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const port = process.env.PORT || 8080;
 //middleware imports
 const logger = require('./middleware/logger');
 const notFound = require('./middleware/404');
+const cors = require('cors');
 //routers
 const weatherRouter = require('./routers/weather.router');
 
 //middleware use
 server.use(logger);
 server.use(weatherRouter);
+server.use(cors());
 //dummy route for testing
 server.get('/', (request, response) => {
   response.send('it works!');
